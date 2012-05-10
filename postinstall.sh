821468b325b445941edfe3cdb6b166affd60e00c
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2012-05-09 19:33:01 -0500

    Install default panel setup entries

---
+++ postinstall.sh
@@ -0,0 +1,3 @@
+/usr/bin/mateconftool-2 --direct --config-source=$(/usr/bin/mateconftool-2 --get-default-source) --load /etc/mateconf/schemas/panel-default-setup.entries
+/usr/bin/mateconftool-2 --direct --config-source=$(/usr/bin/mateconftool-2 --get-default-source) --load /etc/mateconf/schemas/panel-default-setup.entries /apps/panel
+
