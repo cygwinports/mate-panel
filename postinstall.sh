3c4e929470d3a7da44dca228dad42ef28184635c
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2013-04-04 03:18:14 -0500

    mate-panel 1.6.0

--- postinstall.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/mateconftool-2 --direct --config-source=$(/usr/bin/mateconftool-2 --get-default-source) --load /etc/mateconf/schemas/panel-default-setup.entries
-/usr/bin/mateconftool-2 --direct --config-source=$(/usr/bin/mateconftool-2 --get-default-source) --load /etc/mateconf/schemas/panel-default-setup.entries /apps/panel
-
